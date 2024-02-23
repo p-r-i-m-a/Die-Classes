e1a7be6aecd322dbd08a5f346cacc652a99778b8
Domenic Maniacco <dmaniacco3291@gmail.com>
2024-02-23 13:49:08 -0500

    Added extra class and stuff

---
+++ Die Classes/die.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Die_Classes
+{
+    public class Die
+    {
+        private Random _generator;
+        private int _roll;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+
+        public Die(int roll)
+        {
+            _generator = new Random(roll);
+            _generator.Next(1, 7);
+        }
+
+        public int Roll { get { return _roll; } }
+
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+
+        public void RollDie()
+        {
+            
+        }
+    }
+
+
+}
