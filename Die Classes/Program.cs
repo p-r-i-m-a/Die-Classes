a2235a257ff4e8ba91d4a79cdb8a027562b0766d
Domenic Maniacco <dmaniacco3291@gmail.com>
2024-03-04 12:23:31 -0500

    done now

--- Die Classes/Program.cs
+++ Die Classes/Program.cs
@@ -1,179 +1,196 @@
 ﻿using System.Numerics;
-using System.Runtime.CompilerServices;
 
 namespace Die_Classes
 {
     internal class Program
     {
-        public static void gamee()
+        static void Main(string[] args)
         {
             int roll1, roll2, totalRoll, bet;
             double betAmount, wallet = 100;
             bool playing = false, done = false, chosing = false;
-
-            string chose;
-
+            
+            string Continue;
             do
             {
-                Console.WriteLine("Welcome to the dice game, in this game you may bet on the outcome of two dice that will be rolled. \n\rYou have $100 in your 'wallet'.");
-                Console.WriteLine();
                 do
                 {
-                    done = false;
-                    Console.WriteLine("Please enter the amount you want to bet from your wallet:");
+                    Console.WriteLine("Welcome to the dice game, in this game you may bet on the outcome of two dice that will be rolled. \n\rYou have $100 in your 'wallet'.");
                     Console.WriteLine();
-                    betAmount = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine();
-                    if (betAmount <= 0)
-                    {
-                        Console.WriteLine("That bet is below 0.");
-                        Console.WriteLine();
-                    }
-                    else if (betAmount >= wallet)
-                    {
-                        betAmount = wallet;
-                        Console.WriteLine("You have set your bet higher than whats in your wallet. Bet has been set to current wallet max.");
-                        Console.WriteLine();
-                        done = true;
-                    }
-                    else if (betAmount >= 0)
-                    {
-                        done = true;
-                    }
-                } while (!done);
-
-
-
-                do
-                {
-                    done = false;
-                    Console.WriteLine("Please chose a number between 2 and 12: ");
-                    Console.WriteLine();
-                    bet = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine();
-                    if (bet > 12 || bet < 2)
+                    do
                     {
                         done = false;
+                        Console.WriteLine("Please enter the amount you want to bet from your wallet:");
+                        Console.WriteLine();
+                        betAmount = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine();
+                        if (betAmount <= 0)
+                        {
+                            Console.WriteLine("That bet is below 0.");
+                            Console.WriteLine();
+                        }
+                        else if (betAmount >= wallet)
+                        {
+                            betAmount = wallet;
+                            Console.WriteLine("You have set your bet higher than whats in your wallet. Bet has been set to current wallet max.");
+                            Console.WriteLine();
+                            done = true;
+                        }
+                        else if (betAmount >= 0)
+                        {
+                            done = true;
+                        }
+                    } while (!done);
+
+
+
+                    do
+                    {
+                        done = false;
+                        Console.WriteLine("Please chose a number between 2 and 12: ");
+                        Console.WriteLine();
+                        bet = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine();
+                        if (bet > 12 || bet < 2)
+                        {
+                            done = false;
+                        }
+                        else
+                        {
+                            done = true;
+                        }
+
+                    } while (!done);
+
+
+                    Console.WriteLine();
+
+
+
+                    Console.WriteLine("You have bet " + betAmount + "$. You bet on number " + bet);
+                    Console.WriteLine();
+                    Console.WriteLine("If you wish to proceed, enter Y, if you wish to make different bets, enter N.");
+                    Console.WriteLine();
+                    Continue = Console.ReadLine().ToUpper();
+                    done = false;
+
+                    while (done == false)
+                    {
+                        if (Continue == "Y")
+                        {
+                            done = true;
+                        }
+                        else if (Continue == "N")
+                        {
+                            done = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter eiter Y, or N.");
+                            Console.WriteLine();
+                            Continue = Console.ReadLine().ToUpper();
+                            Console.WriteLine();
+                        }
+
+                    }
+
+                    if (Continue == "Y")
+                    {
+                        chosing = true;
                     }
                     else
                     {
+                        chosing = false;
+                    }
+                } while (!chosing);
+
+
+                wallet = wallet - betAmount;
+                Console.Clear();
+                Console.WriteLine("You bet: " + betAmount);
+                Console.WriteLine();
+                Console.WriteLine("You bet on: #" + bet);
+                Console.WriteLine();
+                Console.WriteLine("Press 'enter' to continue.");
+                Console.ReadLine();
+                Console.WriteLine();
+
+                Die die1 = new Die();
+                Die die2 = new Die();
+
+                die1.DrawRoll();
+                die2.DrawRoll();
+
+                Console.WriteLine(die1.Roll);
+                Console.WriteLine();
+                Console.WriteLine(die2.Roll);
+
+                roll1 = Convert.ToInt32(die1.Roll);
+                roll2 = Convert.ToInt32(die2.Roll);
+                totalRoll = roll1 + roll2;
+
+                Console.WriteLine("You rolled a " + totalRoll);
+                Console.WriteLine();
+                if (totalRoll == bet)
+                {
+                    Console.WriteLine("You won!");
+                    if (roll1 == roll2)
+                    {
+                        Console.WriteLine("The rolls where doubles, you earn 2x the initial bet!");
+                        Console.WriteLine();
+                        wallet = wallet + (bet * 2);
+                        Console.WriteLine("Your wallet total is now: $" + wallet);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No doubles rolled, you earn 1x the bet.");
+                        Console.WriteLine();
+                        wallet = wallet + bet;
+                        Console.WriteLine("Your wallet total is now: $" + wallet);
+                    }
+                }
+                else
+                {
+                    wallet = wallet - bet;
+                    Console.WriteLine("You lost. your new wallet total is: $" + wallet);
+                }
+
+                Console.WriteLine("If you want to play again, enter 'Y', if you want to close the program, enter 'N'");
+                Console.WriteLine();
+                Continue = Console.ReadLine().ToUpper();
+                done = false;
+                while (done == false)
+                {
+                    
+                    if (Continue == "Y")
+                    {
                         done = true;
                     }
-
-                } while (!done);
-
-
-                Console.WriteLine();
-
-
-
-                Console.WriteLine("You have bet " + betAmount + "$. You bet on number " + bet);
-                Console.WriteLine();
-                Console.WriteLine("If you wish to proceed, enter Y, if you wish to make different bets, enter N.");
-                Console.WriteLine();
-                chose = Console.ReadLine().ToUpper();
-                Console.WriteLine();
-                while (!playing)
-                {
-                    if (chose == "Y")
+                    else if (Continue == "N")
                     {
-                        playing = true;
-                    }
-                    else if (chose == "N")
-                    {
-                        playing = true;
+                        done = true;
                     }
                     else
                     {
                         Console.WriteLine("Please enter eiter Y, or N.");
                         Console.WriteLine();
-                        chose = Console.ReadLine();
+                        Continue = Console.ReadLine().ToUpper();
                         Console.WriteLine();
-
                     }
-                    
-                    
+
                 }
 
-                if (chose == "N")
+                if (Continue == "Y")
                 {
-                    chosing = false;
-                }
-                else if (chose == "Y")
-                {
-                    chosing = true;
-                }
-            } while (!chosing);
-
-
-            wallet = wallet - betAmount;
-            Console.Clear();
-            Console.WriteLine("You bet: " + betAmount);
-            Console.WriteLine();
-            Console.WriteLine("You bet on: #" + bet);
-            Console.WriteLine();
-            Console.WriteLine("Press 'enter' to continue.");
-            Console.ReadLine();
-            Console.WriteLine();
-
-            Die die1 = new Die();
-            Die die2 = new Die();
-
-            die1.DrawRoll();
-            die2.DrawRoll();
-            Console.WriteLine();
-
-            Console.WriteLine(die1.Roll);
-            Console.WriteLine();
-            Console.WriteLine(die2.Roll);
-
-            roll1 = Convert.ToInt32(die1.Roll);
-            roll2 = Convert.ToInt32(die2.Roll);
-            totalRoll = roll1 + roll2;
-
-            Console.WriteLine("You rolled a " + totalRoll);
-            Console.WriteLine();
-            if (totalRoll == bet)
-            {
-                Console.WriteLine("You won!");
-                if (roll1 == roll2)
-                {
-                    Console.WriteLine("The rolls where doubles, you earn 2x the initial bet!");
-                    Console.WriteLine();
-                    wallet = wallet + (bet * 2);
-                    Console.WriteLine("Your wallet total is now: $" + wallet);
+                    Console.Clear();
+                    playing = true;
                 }
                 else
                 {
-                    Console.WriteLine("No doubles rolled, you earn 1x the bet.");
-                    Console.WriteLine();
-                    wallet = wallet + bet;
-                    Console.WriteLine("Your wallet total is now: $" + wallet);
+                    playing = false;
                 }
-            }
-            else
-            {
-                wallet = wallet - bet;
-                Console.WriteLine("You lost. your new wallet total is: $" + wallet);
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Please enter 'Y' if you want to play again.\r\nPlease enter 'N' if you want to end the program.");
-            chose = Console.ReadLine().ToUpper();
-
-            if (chose == "Y")
-            {
-                Console.Clear();
-                gamee();
-            }
-            else if (chose == "N")
-            {
-                Console.Clear();
-            }
-        }
-        static void Main(string[] args)
-        {
-            gamee();
+            } while (playing);
+            
         }
     }
 }
